d2965631281b53bc338e0cfb5489806b341df27e
JuanCuts <juancuts@gmail.com>
2024-05-16 12:00:21 -0500

    feat: se agrega interfaz teams

---
+++ src/app/shared/models/dbo/team/team.interface.ts
@@ -0,0 +1,10 @@
+export interface TeamModel {
+    teamName: string;
+    points: number;
+    playedMatches: number;
+    wonMatches: number;
+    drawnMatches: number;
+    lostMatches: number;
+    goalsFor: number;
+    goalsAgainst: number;
+}
